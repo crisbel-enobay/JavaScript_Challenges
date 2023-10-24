74dddceb7996f1f4dc9adcf7e4dadd9e0d86158d
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2023-10-24 21:59:13 +0800

    max number

--- traversy-js-challenges/traversy-js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ traversy-js-challenges/traversy-js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,13 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  //   return Math.max(...arr);
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;
