22cf1dac27c8955ade2291292ce1ecc80ff6440f
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2023-10-26 21:54:35 +0800

    title case and reverse str

--- traversy-js-challenges/traversy-js-challenges/01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ traversy-js-challenges/traversy-js-challenges/01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+// const reverseString = (str) => str.split("").reverse().join("");
+
+function reverseString(str) {
+  let reverse = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
+}
 
 module.exports = reverseString;
