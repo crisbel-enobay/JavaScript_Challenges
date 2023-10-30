5e5266e8bf20bf9fe1d1c03c5e67219ff9ded76a
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2023-10-30 21:56:42 +0800

    count vowels

--- traversy-js-challenges/traversy-js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ traversy-js-challenges/traversy-js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,19 @@
-function countVowels() {}
+function countVowels(str) {
+  const formatterStr = str.toLowerCase();
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    const char = formatterStr[i];
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
